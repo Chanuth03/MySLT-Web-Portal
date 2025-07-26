9603c02643e884e245929da2bec21df78d6ef2b4
Chamidu Lakshan <lakshanchamidu994@gmail.com>
2025-07-26 13:22:30 +0530

    Implement mobile screen resp

---
+++ src/Pages/Home.tsx
@@ -0,0 +1,52 @@
+import AccountBalance from "../components/AccountBalance";
+import Banner from "../components/Banner";
+import ContentSection from "../components/ContentSection";
+import AccountSelector from "../components/AccountSelector";
+import CustomNavBar from "../components/CustomNavBar";
+import QuickAccessMenu from "../components/QuickAccessMenu";
+import useStore from "../services/useAppStore";
+import Mobile from "../components/Mobile/Mobile";
+import "./Home.css";
+
+const Home = () => {
+  const { selectedNavbarItem } = useStore();
+
+  return (
+    <div className="home-container">
+      <div className="nav-sidebar">
+        <CustomNavBar />
+      </div>
+
+      {/* Main Content */}
+      <div className="main-content-dashboard">
+        <div className="account-selector-mobile">
+          <AccountSelector />
+        </div>
+
+        <div className="quick-access-mobile">
+          <QuickAccessMenu />
+          <Banner />
+        </div>
+        <div className="top-section">
+          <AccountBalance />
+        </div>
+        <div className="dynamic-content">
+          {selectedNavbarItem === "" && <ContentSection />}
+          {selectedNavbarItem === "Broadband" && <ContentSection />}
+          {selectedNavbarItem === "PeoTV" && <ContentSection />}
+          {selectedNavbarItem === "Voice" && <ContentSection />}
+          {selectedNavbarItem === "Mobile" && <Mobile />}
+        </div>
+      </div>
+      <div className="right-sidebar">
+        <div className="account-selector-desktop">
+          <AccountSelector />
+        </div>
+        <QuickAccessMenu />
+        <Banner />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
